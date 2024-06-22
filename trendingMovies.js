d67ffd17aeae4715edcd3c3005a571189dbb6ced
iamankitsingh267 <78903552+Ankit026-ctrl@users.noreply.github.com>
2024-06-22 13:25:48 +0530

    Update trendingMovies.js

--- trendingMovies.js
+++ trendingMovies.js
@@ -1,23 +1,22 @@
 const trendingMovies = [
-    {
-        title: "Panchayat",
-        description: "Description of Trending Movie 1",
-        imgPath: "images/trending1.jpg",
-        qualityLinks: {
-            '480p': '#',
-            '720p': '#',
-            '1080p': '#'
-        }
+    { 
+        title: "Kota Factory Season 3", 
+        description: "The story is set in Kota, Rajasthan, an educational hub famous for its coaching centres.", 
+        qualityLinks: { '480p': 'https://shrinkme.vip/Kotafactory3_480p', '720p': 'https://shrinkme.vip/KotaFactory3-720p', '1080p': 'https://file-to-link.up.railway.app/watch/66745a5efe5c1d9abbe0f093' }, 
+        imgPath: "https://image.tmdb.org/t/p/original/mQEmKXSFlzBlSoYgut0VfjxAzoA.jpg" 
     },
-    {
-        title: "Trending Movie 2",
-        description: "Description of Trending Movie 2",
-        imgPath: "images/trending2.jpg",
-        qualityLinks: {
-            '480p': '#',
-            '720p': '#',
-            '1080p': '#'
-        }
+    { 
+        title: "Panchayat 3", 
+        description: "Panchayat is an Indian Hindi-language comedy drama streaming television series created by The Viral Fever for Amazon Prime Video.", 
+        qualityLinks: { '480p': 'https://file-to-link.up.railway.app/watch/6672fe8bfe5c1d9abbe0de5b', '720p': 'https://file-to-link.up.railway.app/watch/6672fed7fe5c1d9abbe0de63', '1080p': '#' }, 
+        imgPath: "https://image.tmdb.org/t/p/original/88SrKf1qnkhiCUp4HYkETyWPuoy.jpg" 
+    }
+,
+{ 
+        title: "Gullak", 
+        description: "Gullak’ is back with Season 4! Anchored by their enduring Gullak, the Mishra family tackles work woes, new parenting dilemmas and everyday life with wit and humour.", 
+        qualityLinks: { '480p': 'https://file-to-link.up.railway.app/watch/6673b6fefe5c1d9abbe0e5fb', '720p': 'https://file-to-link.up.railway.app/watch/6673b6fefe5c1d9abbe0e5f9', '1080p': 'https://file-to-link.up.railway.app/watch/6673b6fefe5c1d9abbe0e5fa' }, 
+        imgPath: "https://m.media-amazon.com/images/M/MV5BN2QwZmNhZjctZDZkMi00Mjk2LTg4OWMtNGEzMjk1ZWIwN2IzXkEyXkFqcGdeQXVyNDY4NjAxNTc@._V1_SX300.jpg" 
     }
     // Add more trending movies here
 ];
