b679ce342101765bf60ebe037af0e621d4dfcd81
iamankitsingh267 <78903552+Ankit026-ctrl@users.noreply.github.com>
2024-06-22 13:22:06 +0530

    Update

---
+++ southMovies.js
@@ -0,0 +1,25 @@
+const southMovies = [
+    {
+        title: "South Movie 1",
+        description: "Description of South Movie 1",
+        imgPath: "images/south1.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    },
+    {
+        title: "South Movie 2",
+        description: "Description of South Movie 2",
+        imgPath: "images/south2.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    }
+    // Add more South movies here
+];
+
+export default southMovies;
