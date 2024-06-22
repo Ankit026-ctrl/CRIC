5ac5860cc96405235819804a6ac1187d91992de4
iamankitsingh267 <78903552+Ankit026-ctrl@users.noreply.github.com>
2024-06-22 13:22:29 +0530

    Update

---
+++ hollywoodMovies.js
@@ -0,0 +1,25 @@
+const hollywoodMovies = [
+    {
+        title: "Hollywood Movie 1",
+        description: "Description of Hollywood Movie 1",
+        imgPath: "images/hollywood1.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    },
+    {
+        title: "Hollywood Movie 2",
+        description: "Description of Hollywood Movie 2",
+        imgPath: "images/hollywood2.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    }
+    // Add more Hollywood movies here
+];
+
+export default hollywoodMovies;
