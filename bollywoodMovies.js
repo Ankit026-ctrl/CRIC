76240824364a296c01c6ecd3af9af631c6f1d50f
iamankitsingh267 <78903552+Ankit026-ctrl@users.noreply.github.com>
2024-06-22 13:21:22 +0530

    Update

---
+++ bollywoodMovies.js
@@ -0,0 +1,25 @@
+const bollywoodMovies = [
+    {
+        title: "Bollywood Movie 1",
+        description: "Description of Bollywood Movie 1",
+        imgPath: "images/bollywood1.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    },
+    {
+        title: "Bollywood Movie 2",
+        description: "Description of Bollywood Movie 2",
+        imgPath: "images/bollywood2.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    }
+    // Add more Bollywood movies here
+];
+
+export default bollywoodMovies;
