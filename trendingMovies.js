68ee647d30d4fe8cecb0ab613d87c9eb71e43644
iamankitsingh267 <78903552+Ankit026-ctrl@users.noreply.github.com>
2024-06-22 13:20:49 +0530

    Update

---
+++ trendingMovies.js
@@ -0,0 +1,25 @@
+const trendingMovies = [
+    {
+        title: "Trending Movie 1",
+        description: "Description of Trending Movie 1",
+        imgPath: "images/trending1.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    },
+    {
+        title: "Trending Movie 2",
+        description: "Description of Trending Movie 2",
+        imgPath: "images/trending2.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    }
+    // Add more trending movies here
+];
+
+export default trendingMovies;
