ee50cf9acc68901dd007c616ea7c012a50497e2e
iamankitsingh267 <78903552+Ankit026-ctrl@users.noreply.github.com>
2024-06-22 13:21:46 +0530

    Update

---
+++ webseriesMovies.js
@@ -0,0 +1,25 @@
+const webseriesMovies = [
+    {
+        title: "Webseries 1",
+        description: "Description of Webseries 1",
+        imgPath: "images/webseries1.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    },
+    {
+        title: "Webseries 2",
+        description: "Description of Webseries 2",
+        imgPath: "images/webseries2.jpg",
+        qualityLinks: {
+            '480p': '#',
+            '720p': '#',
+            '1080p': '#'
+        }
+    }
+    // Add more webseries here
+];
+
+export default webseriesMovies;
